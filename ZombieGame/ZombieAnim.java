2b7c0bbbc2c693120f7240c0e910f12927f3e4f2
abdelfattahmostafa <abdelfattahmostafa102@gmail.com>
2025-12-09 21:30:59 +0200

    Update ZombieAnim.java

--- ZombieGame/ZombieAnim.java
+++ ZombieGame/ZombieAnim.java
@@ -1,39 +1,57 @@
 package ZombieGame;
 
-
-import com.cs304.lab9.AnimListener;
 import com.sun.opengl.util.*;
 import java.awt.*;
+import java.awt.event.*;
 import javax.media.opengl.*;
 import javax.swing.*;
 
 public class ZombieAnim extends JFrame {
 
+    
     public static void main(String[] args) {
-        new ZombieAnim();
+        
+        new ZombieGameMenu();
     }
 
-
-    public ZombieAnim() {
+    
+    public ZombieAnim(ZombieGameMenu menuReference, boolean isHardMode, String playerName) {
         GLCanvas glcanvas;
         Animator animator;
 
-        ZombieAnimListener listener = new ZombieGameListener();
+        ZombieGameListener listener = new ZombieGameListener();
+
+        
+        listener.setGameSettings(isHardMode, playerName);
+
         glcanvas = new GLCanvas();
         glcanvas.addGLEventListener(listener);
         glcanvas.addKeyListener(listener);
         getContentPane().add(glcanvas, BorderLayout.CENTER);
+
         animator = new FPSAnimator(15);
         animator.add(glcanvas);
         animator.start();
 
-        setTitle("Zombie Apocalypse");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Zombie Apocalypse - Playing as: " + playerName);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); 
         setSize(700, 700);
         setLocationRelativeTo(null);
         setVisible(true);
         setFocusable(true);
         glcanvas.requestFocus();
+
+        
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                animator.stop(); 
+                UniversalSoundPlayer.stopLoop(); 
+                if (menuReference != null) {
+                    menuReference.setVisible(true); 
+                    menuReference.resetMenuMusic(); 
+                }
+            }
+        });
     }
 }
-
