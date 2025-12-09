5c9c2dedfd2a50f2e954f70d36c40656aa3189a2
abdelfattahmostafa <abdelfattahmostafa102@gmail.com>
2025-12-09 19:11:18 +0200

    Update ZombieAnimListener.java

--- ZombieGame/ZombieAnimListener.java
+++ ZombieGame/ZombieAnimListener.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ZombieGame;
 
 import java.awt.event.KeyListener;
@@ -15,3 +11,4 @@ public abstract class ZombieAnimListener implements GLEventListener, KeyListener
     protected String assetsFolderName = "Game Assets";
 
 }
+
